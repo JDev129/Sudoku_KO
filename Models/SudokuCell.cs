eae1b07a3acbf46837e16992b44e1867b0e07e1b
JDev129 <jchristman129@gmail.com>
2021-09-22 08:20:59 -0400

    Update SudokuCell.cs

--- Models/SudokuCell.cs
+++ Models/SudokuCell.cs
@@ -2,8 +2,6 @@
 {
     public class SudokuCell
     {
-        public int ID { get; set; }
-        public int SudokuMoveID { get; set; }
         public string Value { get; set; }
         public bool IsAStart { get; set; }
     }
